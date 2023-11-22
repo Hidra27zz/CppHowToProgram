14c5a5cab9ef392cee90321ddd59d5d59587d7e6
Hidra27zz <nhan1422hdn@gmail.com>
2023-11-22 12:37:00 +0700

    add .

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,20 +1,21 @@
-// Fig. 20.3: BinarySearch.cpp
-// Binary search of array
+// Fig 20.3: BinarySearch.cpp 
+// Binary search of an array. 
 #include <algorithm>
 #include <array>
-#include <ctime>
-#include <iostream>
-#include <random>
+#include <ctime> 
+#include <iostream> 
+#include <random> 
 using namespace std;
 
 // display array elements from index low through index high
 template <typename T, size_t size>
-void displayElements(const array<T, size>& items, size_t low, size_t high) {
-    for(size_t i{0}; i < items.size() && i < low; ++i) {
-        cout << "   "; // display spaces for alignment
+void displayElements(const array<T, size>& items,
+size_t low, size_t high) {
+    for (size_t i{0}; i < items.size() && i < low; ++i) {
+        cout << "   "; // display spaces for alignment 
     }
-
-    for(size_t i{low}; i < items.size() && i <= high; ++i) {
+        
+    for (size_t i{low}; i < items.size() && i <= high; ++i) {
         cout << items[i] << " "; // display element
     }
     cout << endl;
@@ -29,69 +30,68 @@ int binarySearch(const array<T, size>& items, const T& key) {
     int location{-1}; // key's index; -1 if not found
     do { 
         // loop to search for element
-        // display remaining elements of array to be searched
+        // display remaining elements of array to be searched 
         displayElements(items, low, high);
-
-        // out spaces for alignment
-        for (int i{0}; i < middle; ++i) {
+        
+        // output spaces for alignment
+        for (int i{0}; i < middle; ++i) { 
             cout << "   ";
         }
-        cout << " * " << endl; // indicate current middle
-
+        cout << " * " << endl;
+        // indicate current middle
         // if the element is found at the middle
         if (key == items[middle]) {
             location = middle; // location is the current middle
         }
-        else if (key < items[middle]) { // middle is too high
+        else if (key < items[middle]) { 
+            // middle is too high 
             high = middle - 1; // eliminate the higher half
         }
         else { // middle element is too low
             low = middle + 1; // eliminate the lower half
         }
-
         middle = (low + high + 1) / 2; // recalculate the middle
-    }
+    } 
     while ((low <= high) && (location == -1));
     return location; // return location of key
 }
 
 int main() {
-    // use the default radom-number generation engine to produce uniformly distributed pseudorandom int values from 10 to 99
+    // use the default random-number generation engine to produce
+    // uniformly distributed pseudorandom int values from 10 to 99
     default_random_engine engine{
-        static_cast<unsigned int> (time(nullptr))
-    };
+        static_cast<unsigned int>(time(nullptr))};
+    uniform_int_distribution<unsigned int> randomInt{10,99};
 
-    uniform_int_distribution<unsigned int> randomInt{10, 99};
     const size_t arraySize{15}; // size of array
     array<int, arraySize> arrayToSearch; // create array
-    
     // fill arrayToSearch with random values
     for (int &item : arrayToSearch) {
         item = randomInt(engine);
     }
 
-    sort(arrayToSearch.begin(), arrayToSearch.end()); // sort the array
-
+    sort(arrayToSearch.begin(), arrayToSearch.end());
     // display arrayToSearch's values
     displayElements(arrayToSearch, 0, arrayToSearch.size() - 1);
-    
+
     // get input from user
-    cout << "\nPlease enter an integer value (-1 to quite:) ";
+    cout << "\nPlease enter an integer value (-1 to quit): ";
     int searchKey; // value to locate
     cin >> searchKey; // read an int from user
     cout << endl;
 
     // repeatedly input an integer; -1 terminates the program
     while (searchKey != -1) {
-        /// use binary search to try to find integer
+    // use binary search to try to find integer
         int position{binarySearch(arrayToSearch, searchKey)};
 
-        // return value of -1 indicates integer was not found
+
         if(position == -1) {
-            cout << "The integer " << searchKey << " was not found.\n";
+            cout << "The integer " << searchKey << " was not found.\n"; 
+
         }
         else {
-            cout << "The integer " << searchKey << " was found in position " << position << ".\n";
+            cout << "The integer " << searchKey<< " was found in position " << position << ".\n";
         }
 
         // get input from user
@@ -99,4 +99,5 @@ int main() {
         cin >> searchKey; // read an int from user
         cout << endl;
     }
+    
 }
---
+++ InsertionSort.cpp
@@ -0,0 +1,40 @@
+// Fig. 20.4: InsertionSort.cpp
+// Sorting an array into ascending order with insertion sort.
+#include <array>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+// sort an array into ascending order
+template <typename T, size_t size>
+void insertionSort(array<T, size>& items) {
+    for(size_t next{1}; next < items.size(); ++next) {
+        T insert{items[next]};  // save value of next item to insert
+        size_t moveIndex{next}; // initialize location to place element
+
+        // search for the location in which to put the current element
+        while ((moveIndex > 0) && (items[moveIndex - 1] > insert)) {
+            // shift element one slot to the right
+            items[moveIndex] = items[moveIndex - 1];
+            --moveIndex;
+        }
+        items[moveIndex] = insert; // place insert item back into array
+    }
+}
+
+int main() {
+    const size_t arraySize{10};
+    array<int, arraySize> data{34, 56, 4, 10, 77, 51, 93, 30, 5, 52};
+
+    cout << "Unsorted array:\n";
+    for(size_t i{0}; i < arraySize; i++) {
+        cout << setw(4) << data[i];
+    }
+
+    insertionSort(data);
+    cout << "\nSorted array:\n";
+
+    for(size_t i{0}; i < arraySize; ++i) {
+        cout << setw(4) << data[i];
+    }
+}
